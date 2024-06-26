4c3e202b5f0555b456ac4677bc67692bc31ecea0
amul patle <amulpatle3@gmail.com>
2024-06-26 11:47:55 +0530

    menu page edit

--- menu/models.py
+++ menu/models.py
@@ -30,6 +30,7 @@ class FoodItem(models.Model):
     description = models.TextField(max_length=250,blank=True)
     price = models.DecimalField(max_digits=10,decimal_places=2)
     image = models.ImageField(upload_to='foodimages')
+    is_availabe = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
