603d9ac2e2051629f01c31d03a90f955ef36eade
amul patle <amulpatle3@gmail.com>
2024-07-02 15:49:09 +0530

    display fooditem by category using prefatch function

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 
 from vendor.models import Vendor
-
+from menu.models import Category, FoodItem
+from django.db.models import Prefetch
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True,user__is_active=True)
     vendor_count = vendors.count()
@@ -14,7 +15,15 @@ def marketplace(request):
 
 def vendor_detail(reqeust,vendor_slug):
     vendor = get_object_or_404(Vendor,vendor_slug=vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset = FoodItem.objects.filter(is_availabe=True)
+        )
+    )
+    
     context = {
         'vendor':vendor,
+        'categories':categories,
     }
     return render(reqeust,'marketplace/vendor_detail.html',context)
