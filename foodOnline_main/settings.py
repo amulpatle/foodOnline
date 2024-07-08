9752f5696ddda2176a208f38a928188640434514
amul patle <amulpatle3@gmail.com>
2024-07-08 11:14:16 +0530

    cart counter context processor

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -68,6 +68,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'marketplace.context_processor.get_cart_counter',
+                
             ],
         },
     },
