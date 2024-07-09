903e85f570f0faad0d5f37ce243ffafe871eace6
amul patle <amulpatle3@gmail.com>
2024-07-09 10:59:30 +0530

    cart url setup

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('add_to_cart/<int:food_id>/',views.add_to_cart,name='add_to_cart'),
     #Decrese cart
     path('decrease_cart/<int:food_id>/',views.decrease_cart,name='decrease_cart'),
+   
 ]
