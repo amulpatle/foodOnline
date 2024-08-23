cd040b663fd0a133e25e6b459c9f4eec2883c087
amul patle <amulpatle3@gmail.com>
2024-08-23 14:30:53 +0530

    completed project

--- accounts/utils.py
+++ accounts/utils.py
@@ -80,7 +80,7 @@ def send_notification(mail_subject,mail_template,context):
     
 def send_notification(mail_subject, mail_template, context):
     from_email = settings.DEFAULT_FROM_EMAIL
-    message = render_to_string(mail_template, context)
+    message = render_to_string(mail_template,context)
     if(isinstance(context['to_email'], str)):
         to_email = []
         to_email.append(context['to_email'])
