3b303c0e2250b8e4d3717a4d1182abc31a139ae0
amul patle <amulpatle3@gmail.com>
2024-08-23 16:10:57 +0530

    add allowed host

--- build.sh
+++ build.sh
@@ -1,27 +1,42 @@
+
+
+# # Exit immediately if a command exits with a non-zero status.
+# set -e
+
+# sudo apt-get update
+# sudo apt-get install -y gdal-bin libgdal-dev
+
+# echo "Installing dependencies..."
+# pip install -r requirements.txt
+
+# echo "Applying database migrations..."
+# python manage.py migrate
+
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput
+
+# echo "Running tests..."
+# python manage.py test
+
+# echo "Build completed successfully."
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+# if [[ $CREATE_SUPERUSER ]];
+# then
+#   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+# fi
+
+
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
+# Install system dependencies
+apt-get update && apt-get install -y gdal-bin libgdal-dev
 
-sudo apt-get update
-sudo apt-get install -y gdal-bin libgdal-dev
-
-echo "Installing dependencies..."
+# Install Python dependencies
 pip install -r requirements.txt
 
-echo "Applying database migrations..."
-python manage.py migrate
-
-echo "Collecting static files..."
+# Collect static files
 python manage.py collectstatic --noinput
 
-echo "Running tests..."
-python manage.py test
-
-echo "Build completed successfully."
-python manage.py collectstatic --no-input
+# Apply database migrations
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi
